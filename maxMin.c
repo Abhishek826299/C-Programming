c1ada2bb6af0c150e3f76dbc0b4e4afda8724aff
unknown <abhibhong37@gmail.com>
2025-06-11 13:39:09 +0530

    maxMin

---
+++ maxMin.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter 2 numbers : ");
+    scanf("%d%d",&a,&b);
+
+    if(a>b)
+    printf("Maximum is %d",a);
+
+    else
+    printf("Maximum is %d",b);
+
+    return 0;
+}
