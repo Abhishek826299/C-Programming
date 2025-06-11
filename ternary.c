9f104173b1da8a6f3f9dd021a3fab39c096607ce
unknown <abhibhong37@gmail.com>
2025-06-11 13:27:44 +0530

    ifelse

---
+++ ternary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    (n>0)?printf("Positive Number"):printf("Negative Number");
+
+    return 0;
+}
