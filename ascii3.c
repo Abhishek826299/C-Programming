ba682425cad85e81a8b3b0452e9ac94ba44e0585
unknown <abhibhong37@gmail.com>
2025-06-23 13:31:11 +0530

    loops

---
+++ ascii3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char ch;
+    printf("Enter Anything : ");
+    scanf("%c",&ch);
+
+    if (isalpha(ch))
+    printf("Alphabet");
+
+    else if (isdigit(ch))
+    printf("Digit");
+
+    else
+    printf("Special Symbol");
+
+    return 0;
+
+}
---
+++ switch.c
@@ -0,0 +1,163 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    if(n==1)
+    printf("One");
+
+    else if(n==2)
+    printf("Two");
+
+    else if(n==3)
+    printf("Three");
+
+    else if(n==4)
+    printf("Four");
+
+    else if(n==5)
+    printf("Five");
+
+    else if(n==6)
+    printf("Six");
+
+    else if(n==7)
+    printf("Seven");
+
+    else if(n==8)
+    printf("Eight");
+
+    else if(n==9)
+    printf("Nine");
+
+    else if(n==10)
+    printf("Ten");
+
+    else if(n==11)
+    printf("Eleven");
+
+    else if(n==12)
+    printf("Twelve");
+
+    else if(n==13)
+    printf("Thirteen");
+
+    else if(n==14)
+    printf("Fourteen");
+
+    else if(n==15)
+    printf("Fifteen");
+
+    else if(n==16)
+    printf("Sixteen");
+
+    else if(n==17)
+    printf("Seventeen");
+
+    else if(n==18)
+    printf("Eighteen");
+
+    else if(n==19)
+    printf("Nineteen");
+
+    else if(n==20)
+    printf("Twenty");
+
+    else if(n==21)
+    printf("Twenty-One");
+
+    else if(n==22)
+    printf("Twenty-Two");
+
+    else if(n==23)
+    printf("Twenty-Three");
+
+    else if(n==24)
+    printf("Twenty-Four");
+
+    else if(n==25)
+    printf("Twenty-Five");
+
+    else if(n==26)
+    printf("Twenty-Six");
+
+    else if(n==27)
+    printf("Twenty-Seven");
+
+    else if(n==28)
+    printf("Twenty-Eight");
+
+    else if(n==29)
+    printf("Twenty-Nine");
+
+    else if(n==30)
+    printf("Thirty");
+
+    else if(n==31)
+    printf("Thirty-One");
+
+    else if(n==32)
+    printf("Thirty-Two");
+
+    else if(n==33)
+    printf("Thirty-Three");
+
+    else if(n==34)
+    printf("Thirty-Four");
+
+    else if(n==35)
+    printf("Thirty-Five");
+
+    else if(n==36)
+    printf("Thirty-Six");
+
+    else if(n==37)
+    printf("Thirty-Seven");
+
+    else if(n==38)
+    printf("Thirty-Eight");
+
+    else if(n==39)
+    printf("Thirty-Nine");
+
+    else if(n==40)
+    printf("Fourty");
+
+     else if(n==41)
+    printf("Fourty-One");
+
+    else if(n==42)
+    printf("Fourty-Two");
+
+    else if(n==43)
+    printf("Fourty-Three");
+
+    else if(n==44)
+    printf("Fourty-Four");
+
+    else if(n==45)
+    printf("Fourty-Five");
+
+    else if(n==46)
+    printf("Foiurty-Six");
+
+    else if(n==47)
+    printf("Fourty-Seven");
+
+    else if(n==48)
+    printf("Fourty-Eight");
+
+    else if(n==49)
+    printf("Fourty-Nine");
+
+    else if(n==50)
+    printf("Fifty");
+
+    else
+    printf("Invalid");
+
+    return 0;
+
+}
