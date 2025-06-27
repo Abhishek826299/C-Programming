5cedfd3c703604526db822f30ad1bbd36cf90c16
unknown <abhibhong37@gmail.com>
2025-06-27 16:17:57 +0530

    friPrograms

---
+++ palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,temp,rev=0,rem;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    temp=n;
+    while(n>0)
+    {
+        int rem = n%10;
+        rev = rev * 10 + rem;
+        n = n/10;
+    }
+    if(rev==temp)
+    printf("Palindrome Number");
+
+    else
+    printf("Not Palindrome Number");
+
+    return 0;
+}
